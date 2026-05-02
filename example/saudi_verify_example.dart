e08d40d94823fd76801a71c17cb7db5c71b3e18d
Ali Alawami <alis2012@gmail.com>
2026-05-02 22:36:33 +0300

    Added example, update readMe, and changelog

--- example/saudi_verify_example.dart
+++ example/saudi_verify_example.dart
@@ -1,5 +1,45 @@
 import 'package:saudi_verify/saudi_verify.dart';
 
 void main() {
+  var nationalID = '1234567890';
+  var mobile = '0555555555';
+  var vat = '312345678901233';
+   var iban = 'SA0380000000608010167519';
+
+   // National ID validation
+  var nationalIDResult = validateNationalID(nationalID);
+  switch (nationalIDResult) {
+    case Valid(:final metadata):
+      print('National ID is valid: $metadata');
+    case Invalid(:final reason):
+      print('National ID is invalid: $reason');
+  }
+
+  // Mobile validation
+  var mobileResult = validateMobile(mobile);
+  switch (mobileResult) {
+    case Valid(:final metadata):
+      print('Mobile is valid: $metadata');
+    case Invalid(:final reason):
+      print('Mobile is invalid: $reason');
+  }
+
+  // IBAN validation
+  var ibanResult = validateIBAN(iban);
+  switch (ibanResult) {
+    case Valid(:final metadata):
+      print('IBAN is valid: $metadata');
+    case Invalid(:final reason):
+      print('IBAN is invalid: $reason');
+  }
+
+  // VAT validation
+  var vatResult = validateVAT(vat);
+  switch (vatResult) {
+    case Valid(:final metadata):
+      print('VAT is valid: $metadata');
+    case Invalid(:final reason):
+      print('VAT is invalid: $reason');
+  }
 
 }
--- lib/saudi_verify.dart
+++ lib/saudi_verify.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-// export 'src/saudi_verify_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/validation_result.dart';
+export 'src/national_id.dart';
+export 'src/mobile.dart';
+export 'src/iban.dart';
+export 'src/vat.dart';
