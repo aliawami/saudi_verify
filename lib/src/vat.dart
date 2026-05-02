8b601766e8862ad4df74b79af67f93783510b661
Ali Alawami <alis2012@gmail.com>
2026-05-02 20:50:21 +0300

    Implemented vat validation and its test cases

---
+++ lib/src/vat.dart
@@ -0,0 +1,21 @@
+import 'package:saudi_verify/src/validation_result.dart';
+
+ValidationResult validateVAT(String vat) {
+  if (vat.isEmpty) {
+    return const Invalid("VAT is required");
+  }
+  if (!RegExp(r'^\d+$').hasMatch(vat)) {
+    return const Invalid("VAT must be a number");
+  }
+  if (vat.length != 15) {
+    return const Invalid("VAT must be 15 digits");
+  }
+  if (!vat.startsWith("3")) {
+    return const Invalid("VAT must start with 3");
+  }
+  if (!vat.endsWith("3")) {
+    return const Invalid("VAT must end with 3");
+  }
+
+  return const Valid(metadata: {"issuingAuthority": "ZATCA"});
+}
--- test/vat_test.dart
+++ test/vat_test.dart
@@ -1,11 +1,25 @@
+import 'package:saudi_verify/src/validation_result.dart';
+import 'package:saudi_verify/src/vat.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('VAT Validation', () {
-    // test('Empty entry', () {
-    //   var vat = '';
-    //   var result = validateVAT(vat);
-    //   expect(result, isA<Invalid>());
-    // });
+    test('Empty entry', () {
+      var vat = '';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('Invalid VAT', () {
+      var vat = '123456789012345';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('Valid VAT', () {
+      var vat = '312345678901233';
+      var result = validateVAT(vat);
+      expect(result, isA<Valid>());
+    });
   });
 }
