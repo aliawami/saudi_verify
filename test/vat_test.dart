c267f4abfaf373c72328c28455d5434e6ff4553c
Ali Alawami <alis2012@gmail.com>
2026-05-02 20:55:24 +0300

    Added additional test cases to Vat test

--- test/vat_test.dart
+++ test/vat_test.dart
@@ -21,5 +21,35 @@ void main() {
       var result = validateVAT(vat);
       expect(result, isA<Valid>());
     });
+
+    test('Start with 3 but not end with 3', () {
+      var vat = '312345678901231';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('End with 3 but not start with 3', () {
+      var vat = '112345678901233';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('Wrong length (large)', () {
+      var vat = '3123456789012313';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('Wrong length (small)', () {
+      var vat = '312345012313';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
+
+    test('Vat with alphabetic characters', () {
+      var vat = '312B45678901233';
+      var result = validateVAT(vat);
+      expect(result, isA<Invalid>());
+    });
   });
 }
